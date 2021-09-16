b16f3b34f9fbaa586a1d77fb68b548e8d204fe5d
Ben Robinson <benr@blrobinson.uk>
2021-09-16 09:53:54 +0100

    allow multiple workers to be generated

--- worker.tf
+++ worker.tf
@@ -1,31 +1,43 @@
 resource "libvirt_volume" "worker" {
-  name           = "worker"
+  count = var.worker_count
+  name           = "worker-${count.index}"
   base_volume_id = libvirt_volume.root_cloudinit.id
   size           = 5368709120
 }
 
 data "template_file" "user_data_worker" {
+  count = var.worker_count
   template = file("${path.module}/configs/workers/cloud_init.cfg")
+  vars = {
+    count = count.index
+    worker_count = var.worker_count
+  }
 }
 
 data "template_file" "network_config_worker" {
+  count = var.worker_count
   template = file("${path.module}/configs/workers/network_config.cfg")
+  vars = {
+    count = count.index
+  }
 }
 
 resource "libvirt_cloudinit_disk" "commoninit-worker" {
-  name           = "commoninit-workers.iso"
-  user_data      = data.template_file.user_data_worker.rendered
-  network_config = data.template_file.network_config_worker.rendered
+  count = var.worker_count
+  name           = "commoninit-worker-${count.index}.iso"
+  user_data      = data.template_file.user_data_worker[count.index].rendered
+  network_config = data.template_file.network_config_worker[count.index].rendered
   pool           = libvirt_pool.k8s.name
 }
 
 # Create the machine
 resource "libvirt_domain" "domain-debian-worker" {
-  name   = "k8s-worker"
+  count = var.worker_count
+  name   = "k8s-worker-${count.index}"
   memory = "2048"
   vcpu   = 2
 
-  cloudinit = libvirt_cloudinit_disk.commoninit-worker.id
+  cloudinit = libvirt_cloudinit_disk.commoninit-worker[count.index].id
 
   network_interface {
     macvtap = "bond0"
@@ -44,7 +56,7 @@ resource "libvirt_domain" "domain-debian-worker" {
   }
 
   disk {
-    volume_id = libvirt_volume.worker.id
+    volume_id = libvirt_volume.worker[count.index].id
   }
 
   graphics {
