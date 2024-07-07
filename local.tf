46639c0633ae0acbd5388f6a6bd2b730048a5210
Ben Robinson <benr@blrobinson.uk>
2024-07-07 13:45:14 +0100

    Settle memory usage

--- local.tf
+++ local.tf
@@ -1,11 +1,11 @@
 locals {
   hv1_workers = {
-    k8s-worker-1 = { ip = "10.0.0.21", memory = "3379" }
-    k8s-worker-2 = { ip = "10.0.0.22", memory = "3379" }
+    k8s-worker-1 = { ip = "10.0.0.21", memory = "3072" }
+    k8s-worker-2 = { ip = "10.0.0.22", memory = "3072" }
   }
   hv2_workers = {
-    k8s-worker-3 = { ip = "10.0.0.23", memory = "7372" }
-    k8s-worker-4 = { ip = "10.0.0.24", memory = "7372" }
+    k8s-worker-3 = { ip = "10.0.0.23", memory = "7168" }
+    k8s-worker-4 = { ip = "10.0.0.24", memory = "7168" }
   }
   total_worker_count = 4
 }
